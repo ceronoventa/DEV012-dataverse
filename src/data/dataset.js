ec97932d32a5375c15222833877c4528e5f2cc6d
Camila Palma <cpalma.contacto@gmail.com>
2023-10-31 11:01:33 -0300

    avance11am

--- src/data/dataset.js
+++ src/data/dataset.js
@@ -5,7 +5,7 @@ export default [
         name: "Bestia",
         shortDescription: "Mutante con habilidades físicas sobrehumanas y un intelecto genio.",
         description: "Bestia es conocido por su fuerza sobrehumana, agilidad y un intelecto brillante. Ha sido un valioso miembro de los X-Men.",
-        imageUrl: "URL(imagesdata/bestia.jpg)",
+        imageUrl: "./imagesdata/bestia.jpg",
         facts: {
           alignment: "Héroe (X-Men)",
         }
@@ -15,7 +15,7 @@ export default [
         name: "Bishop",
         shortDescription: "Mutante capaz de absorber y redirigir la energía.",
         description: "Bishop posee la capacidad única de absorber la energía y redirigirla como poderosos ataques. Es un defensor de la paz entre mutantes y humanos.",
-        imageUrl: "URL(imagesdata/Bishop.jpg)",
+        imageUrl: "./imagesdata/Bishop.jpg",
         facts: {
           alignment: "Héroe (X-Men)",
         }
@@ -25,7 +25,7 @@ export default [
         name: "Blink",
         shortDescription: "Mutante con la capacidad de crear portales teleportadores.",
         description: "Blink es conocida por su habilidad para abrir portales teleportadores, permitiendo viajes instantáneos a través de distancias.",
-        imageUrl: "URL(imagesdata/blink.jpg)",
+        imageUrl: "./imagesdata/blink.jpg",
         facts: {
           alignment: "Héroe (X-Men)",
         }
@@ -35,9 +35,9 @@ export default [
         name: "Bolivar Trask",
         shortDescription: "Científico y creador de los Centinelas, máquinas diseñadas para cazar mutantes.",
         description: "Bolivar Trask es un científico obsesionado con la creación de los Centinelas, máquinas destinadas a contener a los mutantes. Su papel es complejo y a menudo antagónico.",
-        imageUrl: "URL(imagesdata/BolivarTrask.jpg)",
+        imageUrl: "./imagesdata/BolivarTrask.jpg",
         facts: {
-          alignment: "Neutral (Antagonista de los mutantes en algunas historias)",
+          alignment: "Neutral",
         }
       },
       {
@@ -45,9 +45,9 @@ export default [
         name: "Centinela",
         shortDescription: "Máquinas gigantes creadas para cazar y contener mutantes.",
         description: "Los Centinelas son máquinas gigantes creadas por Bolivar Trask con el propósito de cazar y contener a los mutantes. Son un peligro constante en el mundo de los X-Men.",
-        imageUrl: "URL(imagesdata/Centinela.jpg)",
+        imageUrl: "./imagesdata/Centinela.jpg",
         facts: {
-          alignment: "Neutral (Instrumento de caza de mutantes)",
+          alignment: "Neutral",
         }
       },
       {
@@ -55,7 +55,7 @@ export default [
         name: "Coloso",
         shortDescription: "Puede convertir su cuerpo en acero orgánico.",
         description: "Coloso tiene la habilidad única de transformar su cuerpo en acero orgánico, proporcionando una resistencia y fuerza excepcionales.",
-        imageUrl: "URL(imagesdata/Colossus.jpg)",
+        imageUrl: "./imagesdata/Colossus.jpg",
         facts: {
           alignment: "Héroe (X-Men)",
         }
@@ -65,7 +65,7 @@ export default [
         name: "Havok",
         shortDescription: "Hermano de Cíclope con la capacidad de controlar la energía cósmica.",
         description: "Havok, hermano de Cíclope, puede manipular y controlar la energía cósmica para diversos fines, convirtiéndose en un valioso miembro de los X-Men.",
-        imageUrl: "URL(imagesdata/Havok.jpg)",
+        imageUrl: "./imagesdata/Havok.jpg",
         facts: {
           alignment: "Héroe (X-Men)",
         }
@@ -75,7 +75,7 @@ export default [
         name: "Iceman",
         shortDescription: "Puede crear hielo y bajar la temperatura a su alrededor.",
         description: "Iceman tiene el poder de manipular el hielo, creando estructuras heladas y reduciendo la temperatura en su entorno.",
-        imageUrl: "",
+        imageUrl: "./imagesdata/Iceman.jpg",
         facts: {
           alignment: "Héroe (X-Men)",
         }
@@ -85,7 +85,7 @@ export default [
         name: "Kitty Pryde",
         shortDescription: "Puede atravesar objetos sólidos y es experta en computadoras.",
         description: "Kitty Pryde, también conocida como Shadowcat, tiene la habilidad única de atravesar objetos sólidos y es una experta en sistemas informáticos.",
-        imageUrl: "URL_DE_LA_IMAGEN_KITTY_PRYDE",
+        imageUrl: "./imagesdata/KittyPryde.jpg",
         facts: {
           alignment: "Héroe (X-Men)",
         }
@@ -95,9 +95,9 @@ export default [
         name: "Magneto",
         shortDescription: "Maestro del magnetismo y líder de la Hermandad de Mutantes.",
         description: "Magneto es un poderoso mutante con la capacidad de controlar el magnetismo. A veces líder de la Hermandad de Mutantes, su relación con los X-Men es compleja.",
-        imageUrl: "URL_DE_LA_IMAGEN_MAGNETO",
+        imageUrl: "./imagesdata/Magneto.jpg",
         facts: {
-          alignment: "Villano (En algunas historias)",
+          alignment: "Villano",
         }
       },
       {
@@ -105,19 +105,19 @@ export default [
         name: "Gambito",
         shortDescription: "Capaz de cargar objetos con energía explosiva y experto en combate.",
         description: "Gambito posee la habilidad de cargar objetos con energía explosiva y es un hábil luchador cuerpo a cuerpo. Es un valioso miembro de los X-Men.",
-        imageUrl: "URL_DE_LA_IMAGEN_GAMBITO",
+        imageUrl: "./imagesdata/Gambito.jpg",
         facts: {
           alignment: "Héroe (X-Men)",
         }
       },
       {
-        id: "jean-grey-mystique",
-        name: "Jean Grey / Mystique",
-        shortDescription: "Jean Grey es una poderosa telepata y telequinética, Mystique puede cambiar de forma.",
-        description: "Jean Grey es conocida por su poder telepático y telequinético, mientras que Mystique tiene la capacidad única de cambiar su apariencia física a voluntad.",
-        imageUrl: "URL_DE_LA_IMAGEN_JEAN_GREY_MYSTIQUE",
+        id: "mystique",
+        name: "Mystique",
+        shortDescription: "Mystique puede cambiar de forma.",
+        description: "Mystique tiene la capacidad única de cambiar su apariencia física a voluntad.",
+        imageUrl: "./imagesdata/Jean grey   Mystique.jpg",
         facts: {
-          alignment: "Héroe (Jean Grey), Neutral (Mystique)",
+          alignment: "Neutral",
         }
       },
       {
@@ -125,9 +125,9 @@ export default [
         name: "Profesor X",
         shortDescription: "Líder de los X-Men y poderoso telepata.",
         description: "Profesor X es el fundador y líder de los X-Men, con habilidades telepáticas extraordinarias. Su visión es unir a mutantes y humanos en armonía.",
-        imageUrl: "URL_DE_LA_IMAGEN_PROFESOR_X",
+        imageUrl: "./imagesdata/Profesor X.jpg",
         facts: {
-          alignment: "Héroe (Líder de los X-Men)",
+          alignment: "Héroe",
         }
       },
       {
@@ -135,7 +135,7 @@ export default [
         name: "Quicksilver",
         shortDescription: "Mutante con supervelocidad.",
         description: "Quicksilver posee la capacidad de moverse a velocidades increíbles, superando cualquier límite humano. Es un activo miembro de los X-Men.",
-        imageUrl: "URL_DE_LA_IMAGEN_QUICKSILVER",
+        imageUrl: "./imagesdata/Quicksilver.jpg",
         facts: {
           alignment: "Héroe (X-Men)",
         }
@@ -145,7 +145,7 @@ export default [
         name: "Sunspot",
         shortDescription: "Puede absorber y canalizar la energía solar para aumentar su fuerza.",
         description: "Sunspot tiene la capacidad de absorber la energía solar y canalizarla para aumentar su fuerza física. Es un miembro valioso de los X-Men.",
-        imageUrl: "URL_DE_LA_IMAGEN_SUNSPOT",
+        imageUrl: "./imagesdata/Sunspot.jpg",
         facts: {
           alignment: "Héroe (X-Men)",
         }
@@ -155,9 +155,9 @@ export default [
         name: "Toad",
         shortDescription: "Mutante con habilidades físicas mejoradas, incluida una lengua prensil.",
         description: "Toad posee habilidades físicas mejoradas, incluida una lengua prensil que utiliza en combate. Aunque ha sido un antagonista, su historia es compleja.",
-        imageUrl: "URL_DE_LA_IMAGEN_TOAD",
+        imageUrl: "./imagesdata/Toad.jpg",
         facts: {
-          alignment: "Villano (En algunas historias)",
+          alignment: "Villano",
         }
       },
       {
@@ -165,7 +165,7 @@ export default [
         name: "Tormenta",
         shortDescription: "Controla el clima y es una de las líderes de los X-Men.",
         description: "Tormenta tiene la capacidad de controlar y manipular el clima, y ha sido una líder destacada en los X-Men. Su presencia es fundamental en el equipo.",
-        imageUrl: "URL_DE_LA_IMAGEN_TORMENTA",
+        imageUrl: "./imagesdata/Tormenta.jpg",
         facts: {
           alignment: "Héroe (X-Men)",
         }
@@ -175,7 +175,7 @@ export default [
         name: "Warpath",
         shortDescription: "Mutante con habilidades físicas mejoradas y habilidad de vuelo.",
         description: "Warpath posee habilidades físicas mejoradas y la capacidad de volar, convirtiéndose en un formidable miembro de los X-Men.",
-        imageUrl: "URL_DE_LA_IMAGEN_WARPATH",
+        imageUrl: "./imagesdata/Warpath.jpg",
         facts: {
           alignment: "Héroe (X-Men)",
         }
@@ -185,9 +185,9 @@ export default [
         name: "William Stryker",
         shortDescription: "Humano, militar y líder anti-mutante.",
         description: "William Stryker es un líder militar anti-mutante que ve a los mutantes como una amenaza. Su papel a menudo lo coloca en conflicto con los X-Men.",
-        imageUrl: "URL_DE_LA_IMAGEN_STRYKER",
+        imageUrl: "./imagesdata/William Stryker.jpg",
         facts: {
-          alignment: "Villano (En algunas historias)",
+          alignment: "Villano",
         }
       },
       {
@@ -195,7 +195,7 @@ export default [
         name: "Wolverine",
         shortDescription: "Tiene garras retráctiles y sentidos agudos.",
         description: "Wolverine, también conocido como Logan, es conocido por sus garras retráctiles, sentidos agudos y habilidades regenerativas. Es un miembro fundamental de los X-Men.",
-        imageUrl: "URL_DE_LA_IMAGEN_WOLVERINE",
+        imageUrl: "./imagesdata/Wolverine.jpg",
         facts: {
           alignment: "Héroe (X-Men)",
         }
@@ -205,7 +205,7 @@ export default [
         name: "Emma Frost",
         shortDescription: "Telepata y antigua villana convertida en X-Men.",
         description: "Emma Frost es una poderosa telepata que ha pasado de ser una villana a ser una valiosa miembro de los X-Men. Su habilidad telepática es inigualable.",
-        imageUrl: "URL_DE_LA_IMAGEN_EMMA_FROST",
+        imageUrl: "./imagesdata/emma frost.jpg",
         facts: {
           alignment: "Héroe (X-Men)",
         }
@@ -215,7 +215,7 @@ export default [
         name: "Rogue",
         shortDescription: "Tiene la habilidad de absorber los poderes, recuerdos y habilidades de cualquier persona que toque.",
         description: "Rogue es un personaje de los X-Men con la capacidad de absorber los poderes, recuerdos y habilidades de cualquier persona que toque, lo que la ha llevado a luchar con problemas de control y conexión emocional. A pesar de los desafíos que enfrenta debido a su poder, Rogue es una miembro valiosa de los X-Men y ha desempeñado un papel crucial en numerosas misiones y batallas.",
-        imageUrl: "URL_DE_LA_IMAGEN_ROGUE",
+        imageUrl: "./imagesdata/rogue.jpg",
         facts: {
           alignment: "Héroe (X-Men)",
         }
@@ -225,21 +225,10 @@ export default [
         name: "Nightcrawler",
         shortDescription: "Puede teletransportarse y tiene una apariencia azul y cola.",
         description: "Nightcrawler tiene la habilidad de teletransportarse a cualquier lugar que pueda visualizar. Su apariencia azul y cola lo hacen inconfundible.",
-        imageUrl: "URL_DE_LA_IMAGEN_NIGHTCRAWLER",
+        imageUrl: "./imagesdata/Nightcrawler.jpg",
         facts: {
           alignment: "Héroe (X-Men)",
         }
       },
-      {
-        id: "mystique",
-        name: "Mystique",
-        shortDescription: "Mystique es una mutante con la capacidad única de cambiar su apariencia física.",
-        description: "Mystique es conocida por su habilidad para cambiar su forma física a voluntad. Su agilidad y habilidades la convierten en una oponente formidable.",
-        imageUrl: "URL_DE_LA_IMAGEN_MYSTIQUE",
-        facts: {
-          alignment: "Neutral",
-        }
-      },
-    
 ];
 
--- src/view.js
+++ src/view.js
@@ -1,14 +1,14 @@
 export const renderItems = (data) => {
   console.log(data)
-  // Aquí comienza tu código y puedes retornar lo que tu necesites
-  // Aquí comienza tu código y puedes retornar lo que tu necesites
-  return 'example';
+ 
   // Aquí creamos un string para almacenar las tarjetas de personajes.
   let tarjetas = "";
   // Recorremos el arreglo de datos para generar una tarjeta para cada personaje.
-  for (let i = 0; i < data[0].length; i++) {
-    const personaje = data[0][i];
-    tarjetas += `
+  data.forEach(personaje => {
+    
+    tarjetas += ` 
+    <li>
+
       <dl itemscope itemtype="https://schema.org/Superhero">
         <img src="${personaje.imageUrl}" alt="${personaje.name}" itemprop="image" />
         <dt>Nombre:</dt><dd itemprop="name">${personaje.name}</dd>
@@ -17,8 +17,10 @@ export const renderItems = (data) => {
         <dt>Género:</dt><dd itemprop="gender">${personaje.facts.gender}</dd>
         <dt>Poderes:</dt><dd itemprop="powers">${personaje.facts.powers}</dd>
       </dl>
+      </li>
     `;
   }
+  )
   // Finalmente, retornamos el conjunto de tarjetas como una cadena HTML.
-  return tarjetas;
+  return `<ul>${tarjetas}</ul>`;
 };
