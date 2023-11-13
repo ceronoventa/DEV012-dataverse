f8b73e4c44a12edaad5432eb2e12b34c3b89e508
Camila Palma <cpalma.contacto@gmail.com>
2023-11-12 22:57:43 -0300

    domingodoce

--- src/main.js
+++ src/main.js
@@ -8,26 +8,24 @@ import data from './data/dataset.js';
 
 const root = document.querySelector('#root'); //id contenedor de tarjetas
 root.innerHTML = renderItems(data);
+const manipulatedData = data
 //root contenedor lista de elementos
 //Filtro alineación escribirlo en ingles
 const selectFiltro = document.querySelector("select[name='alignment']")
 selectFiltro.addEventListener("change",(event)=>{
-  const dataFiltrada = filterData(data, "alignment", event.target.value) 
+  const dataFiltrada = filterData(manipulatedData, "alignment", event.target.value) 
   root.innerHTML = renderItems(dataFiltrada);
 
 });
 
 const selectOrden = document.querySelector("select[name='name']")
-selectOrden.addEventListener("change",(event)=>{
-  const orden = sortData(data, "name", event.target.value)
+selectOrden.addEventListener("change",(event)=> {
+  const orden = sortData(manipulatedData, "name", event.target.value)
   root.innerHTML = renderItems(orden);
 });
 
 //boton limpiar
-const clearButton = document.querySelector("button[data-testid='button-clear']");
-clearButton.addEventListener("click", () => {
-  //reestablece el valor de AMBOS 
-  selectFiltro.value = "";
-  selectOrden.value = "";
+const clearButton = document.querySelector('[data-testid="button-clear"]');
+clearButton.addEventListener('click', function () {
   root.innerHTML = renderItems(data);
 });
