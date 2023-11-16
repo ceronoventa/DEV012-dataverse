751ca404b7cb06296808cb077c175a2f6ca8105c
Camila Palma <cpalma.contacto@gmail.com>
2023-11-15 23:23:49 -0300

    tarjetasmasgrandesyfacts

--- src/data/dataset.js
+++ src/data/dataset.js
@@ -1,134 +1,98 @@
 export default [
 
   {
-    id: "bestia",
-    name: "Bestia",
-    shortDescription: "Mutante con habilidades físicas sobrehumanas y un intelecto genio.",
-    description: "Bestia es conocido por su fuerza sobrehumana, agilidad y un intelecto brillante. Ha sido un valioso miembro de los X-Men.",
-    imageUrl: "./imagesdata/beast.jpg",
-    facts: {
-      alignment: "Héroe (X-Men)",
-      gender: "Masculino",
-      powers: "Habilidades físicas sobrehumanas, inteligencia genial."
-    }
-  },
-  {
-    id: "bishop",
-    name: "Bishop",
-    shortDescription: "Mutante capaz de absorber y redirigir la energía.",
-    description: "Bishop posee la capacidad única de absorber la energía y redirigirla como poderosos ataques. Es un defensor de la paz entre mutantes y humanos.",
-    imageUrl: "./imagesdata/Bishop.jpg",
-    facts: {
-      alignment: "Héroe (X-Men)",
-      gender: "Masculino",
-      powers: "Absorción y redirección de energía."
-    }
-  },
-  {
-    id: "blink",
-    name: "Blink",
-    shortDescription: "Mutante con la capacidad de crear portales teleportadores.",
-    description: "Blink es conocida por su habilidad para abrir portales teleportadores, permitiendo viajes instantáneos a través de distancias.",
-    imageUrl: "./imagesdata/blink2.jpg",
-    facts: {
-      alignment: "Héroe (X-Men)",
-      gender: "Femenino", 
-      powers: "Creación de portales teleportadores."
-    }
-  },
-  {
+    
     id: "bolivar-trask",
     name: "Bolivar Trask",
     shortDescription: "Científico y creador de los Centinelas, máquinas diseñadas para cazar mutantes.",
-    description: "Bolivar Trask es un científico obsesionado con la creación de los Centinelas, máquinas destinadas a contener a los mutantes. Su papel es complejo y a menudo antagónico.",
+    description: "Bolivar Trask es un genio científico obsesionado con los Centinelas, máquinas diseñadas para contener mutantes. Su papel oscila entre la neutralidad y la antagonía, liderando su misión anti-mutante. Con mente brillante, Trask se enfrenta constantemente a los X-Men, desencadenando conflictos que exploran las complejidades morales de la coexistencia mutante-humana.",
     imageUrl: "./imagesdata/Bolivar Trask.jpg",
     facts: {
       alignment: "Neutral",
-      gender: "Masculino", // Agregado
-      powers: "Genio científico" // Agregado
+      gender: "Masculino",
+      powers: "Genio científico"
     }
   },
   {
     id: "centinela",
     name: "Centinela",
     shortDescription: "Máquinas gigantes creadas para cazar y contener mutantes.",
-    description: "Los Centinelas son máquinas gigantes creadas por Bolivar Trask con el propósito de cazar y contener a los mutantes. Son un peligro constante en el mundo de los X-Men.",
+    description: "Los Centinelas, creación de Bolivar Trask, son colosos mecánicos diseñados para cazar y contener mutantes. Estas máquinas avanzadas poseen habilidades de combate superiores y representan una amenaza constante para los X-Men. Aunque su alineación es neutral, su implementación como armas anti-mutantes a menudo genera conflicto y desafía la visión de convivencia pacífica entre humanos y mutantes.",
     imageUrl: "./imagesdata/centinela2.jpg",
     facts: {
       alignment: "Neutral",
-      gender: "ninguno",
-      powers: "Habilidades de combate avanzadas" // Agregado
+      abilities: "Habilidades de combate avanzadas"
     }
   },
   {
     id: "coloso",
     name: "Coloso",
     shortDescription: "Puede convertir su cuerpo en acero orgánico.",
-    description: "Coloso tiene la habilidad única de transformar su cuerpo en acero orgánico, proporcionando una resistencia y fuerza excepcionales.",
+    description: "Coloso, también conocido como Piotr Rasputin, es un héroe mutante con la capacidad de transformar su cuerpo en acero orgánico, otorgándole fuerza y resistencia sobresalientes. Su alineación con los X-Men lo presenta como un defensor comprometido de la paz entre mutantes y humanos. Coloso, con su imponente presencia y corazón noble, se erige como un pilar esencial en la lucha contra las amenazas a la coexistencia.",
     imageUrl: "./imagesdata/Colossus.jpg",
     facts: {
       alignment: "Héroe (X-Men)",
-      gender: "Masculino", // Agregado
-      powers: "Transformación en acero orgánico" // Agregado
+      gender: "Masculino",
+      powers: "Transformación en acero orgánico"
     }
   },
   {
     id: "havok",
     name: "Havok",
     shortDescription: "Hermano de Cíclope con la capacidad de controlar la energía cósmica.",
-    description: "Havok, hermano de Cíclope, puede manipular y controlar la energía cósmica para diversos fines, convirtiéndose en un valioso miembro de los X-Men.",
+    description: "Havok, cuyo nombre real es Alex Summers, se destaca como un héroe mutante con el poder de controlar la energía cósmica. Hermano de Cíclope, Havok se une a los X-Men, aportando sus habilidades únicas a la lucha por la igualdad y la comprensión entre mutantes y humanos. Su alineación como héroe subraya su papel vital en la búsqueda de la armonía en un mundo lleno de desafíos sobrenaturales.",
     imageUrl: "./imagesdata/havok2.jpg",
     facts: {
       alignment: "Héroe (X-Men)",
-      gender: "Masculino", // Agregado
-      powers: "Manipulación de energía cósmica" // Agregado
+      gender: "Masculino",
+      powers: "Manipulación de energía cósmica"
     }
   },
   {
     id: "iceman",
     name: "Iceman",
     shortDescription: "Puede crear hielo y bajar la temperatura a su alrededor.",
-    description: "Iceman tiene el poder de manipular el hielo, creando estructuras heladas y reduciendo la temperatura en su entorno.",
+    description: "Iceman, también conocido como Bobby Drake, es un héroe mutante con la capacidad de manipular el hielo, creando estructuras heladas y reduciendo la temperatura en su entorno. Su alineación con los X-Men lo presenta como un defensor de la paz y la justicia. Iceman, con su poder refrescante y personalidad vibrante, desafía estereotipos y demuestra la diversidad de talentos dentro de los mutantes.",
     imageUrl: "./imagesdata/iceman2.jpg",
     facts: {
       alignment: "Héroe (X-Men)",
-      gender: "Masculino", // Agregado
-      powers: "Manipulación del hielo" // Agregado
+      gender: "Masculino",
+      powers: "Manipulación del hielo"
     }
   },
   {
     id: "kitty-pryde",
     name: "Kitty Pryde",
     shortDescription: "Puede atravesar objetos sólidos y es experta en computadoras.",
-    description: "Kitty Pryde, también conocida como Shadowcat, tiene la habilidad única de atravesar objetos sólidos y es una experta en sistemas informáticos.",
+    description: "Kitty Pryde, también conocida como Shadowcat, es una heroína mutante con la habilidad única de atravesar objetos sólidos y una destreza excepcional en sistemas informáticos. Su alineación con los X-Men la posiciona como una valiosa miembro del equipo, aportando habilidades versátiles y una perspectiva única. Kitty Pryde, con su ingenio y agilidad, representa la capacidad de adaptación crucial en la lucha por la coexistencia pacífica.",
     imageUrl: "./imagesdata/kitty2.jpg",
     facts: {
       alignment: "Héroe (X-Men)",
-      gender: "Femenino", // Agregado
-      powers: "Intangibilidad y habilidades informáticas" // Agregado
+      gender: "Femenino",
+      powers: "Intangibilidad y habilidades informáticas"
     }
   },
   {
     id: "magneto",
     name: "Magneto",
     shortDescription: "Maestro del magnetismo y líder de la Hermandad de Mutantes.",
-    description: "Magneto es un poderoso mutante con la capacidad de controlar el magnetismo. A veces líder de la Hermandad de Mutantes, su relación con los X-Men es compleja.",
+    description: "Magneto, cuyo nombre real es Erik Lehnsher, es un poderoso mutante con el control absoluto del magnetismo. Su alineación como villano refleja su visión radical de la supremacía mutante. A veces líder de la Hermandad de Mutantes, Magneto desafía a los X-Men con sus métodos y objetivos. Su historia compleja e intransigente destaca las tensiones fundamentales en la lucha por la convivencia.",
     imageUrl: "./imagesdata/magneto2.jpg",
     facts: {
       alignment: "Villano",
-      gender: "Masculino", // Agregado
-      powers: "Control del magnetismo" // Agregado
+      gender: "Masculino",
+      powers: "Control del magnetismo"
     }
   },
   {
     id: "gambito",
     name: "Gambito",
     shortDescription: "Capaz de cargar objetos con energía explosiva y experto en combate.",
-    description: "Gambito posee la habilidad de cargar objetos con energía explosiva y es un hábil luchador cuerpo a cuerpo. Es un valioso miembro de los X-Men.",
+    description: "Gambito, también conocido como Remy LeBeau, es un héroe mutante con la habilidad de cargar objetos con energía explosiva y destreza en combate cuerpo a cuerpo. Su alineación con los X-Men lo presenta como un valioso miembro del equipo, aportando un enfoque táctico y poder explosivo. Gambito, con su encanto y habilidades letales, personifica la diversidad de talentos que definen a los mutantes en su lucha por la aceptación.",
     imageUrl: "./imagesdata/gambito2.jpg",
     facts: {
       alignment: "Héroe (X-Men)",
-      gender: "Masculino", // Agregado
+      gender: "Masculino",
       powers: "Carga de objetos con energía explosiva"
     }
   },
@@ -136,7 +100,7 @@ export default [
     id: "mystique",
     name: "Mystique",
     shortDescription: "Mystique puede cambiar de forma.",
-    description: "Mystique tiene la capacidad única de cambiar su apariencia física a voluntad.",
+    description: "Mystique, cuyo nombre real es Raven Darkholme, es una mutante neutral con la capacidad única de cambiar su apariencia física a voluntad. Su habilidad de mimetismo y cambio de forma la convierte en una figura intrigante y escurridiza. Aunque su alineación es neutral, Mystique ha desempeñado roles diversos, desde antagonista hasta aliada, contribuyendo a la complejidad del mundo mutante.",
     imageUrl: "./imagesdata/mystique2.jpg",
     facts: {
       alignment: "Neutral",
@@ -148,7 +112,7 @@ export default [
     id: "profesor-x",
     name: "Profesor X",
     shortDescription: "Líder de los X-Men y poderoso telepata.",
-    description: "Profesor X es el fundador y líder de los X-Men, con habilidades telepáticas extraordinarias. Su visión es unir a mutantes y humanos en armonía.",
+    description: "Profesor X, cuyo nombre real es Charles Xavier, es el fundador y líder de los X-Men, con habilidades telepáticas extraordinarias. Su alineación como héroe destaca su visión de unir a mutantes y humanos en armonía. A través de su sabiduría y liderazgo, Profesor X guía a los X-Men en la lucha contra la discriminación y la comprensión mutua, enfrentándose a desafíos que van más allá de la fuerza física.",
     imageUrl: "./imagesdata/profesorx2.jpg",
     facts: {
       alignment: "Héroe",
@@ -160,7 +124,7 @@ export default [
     id: "quicksilver",
     name: "Quicksilver",
     shortDescription: "Mutante con supervelocidad.",
-    description: "Quicksilver posee la capacidad de moverse a velocidades increíbles, superando cualquier límite humano. Es un activo miembro de los X-Men.",
+    description: "Quicksilver, cuyo nombre real es Pietro Maximoff, es un héroe mutante con la capacidad extraordinaria de moverse a velocidades increíbles, superando límites humanos. Su alineación con los X-Men lo convierte en un activo miembro del equipo, destacando en situaciones que demandan velocidad y agilidad. Quicksilver, con su carácter impulsivo y velocidad relámpago, añade una dinámica única a la lucha por la coexistencia.",
     imageUrl: "./imagesdata/quicksilver2.jpg",
     facts: {
       alignment: "Héroe (X-Men)",
@@ -172,7 +136,7 @@ export default [
     id: "sunspot",
     name: "Sunspot",
     shortDescription: "Puede absorber y canalizar la energía solar para aumentar su fuerza.",
-    description: "Sunspot tiene la capacidad de absorber la energía solar y canalizarla para aumentar su fuerza física. Es un miembro valioso de los X-Men.",
+    description: "Sunspot, alias Roberto da Costa, destaca entre los X-Men por su capacidad única para absorber la energía solar y canalizarla, potenciando su fuerza física. Como héroe comprometido con la causa mutante, Sunspot desempeña un papel vital en la lucha contra las amenazas que enfrenta el equipo. Su dominio sobre la energía solar lo convierte en un miembro valioso y poderoso, resaltando la diversidad de habilidades entre los mutantes y reforzando el mensaje de unidad que los X-Men defienden.",
     imageUrl: "./imagesdata/sunspot2.jpg",
     facts: {
       alignment: "Héroe (X-Men)",
@@ -184,7 +148,7 @@ export default [
     id: "toad",
     name: "Toad",
     shortDescription: "Mutante con habilidades físicas mejoradas, incluida una lengua prensil.",
-    description: "Toad posee habilidades físicas mejoradas, incluida una lengua prensil que utiliza en combate. Aunque ha sido un antagonista, su historia es compleja.",
+    description: "Toad, conocido como Mortimer Toynbee, es un mutante con habilidades físicas mejoradas y una lengua prensil que utiliza en combate. Aunque ha sido un antagonista en ocasiones, la historia de Toad es compleja, reflejando la lucha interna de muchos mutantes. Con habilidades únicas y una participación variable en la línea entre héroes y villanos, Toad aporta matices intrigantes al universo de los X-Men.",
     imageUrl: "./imagesdata/toad2.jpg",
     facts: {
       alignment: "Villano",
@@ -196,7 +160,7 @@ export default [
     id: "tormenta",
     name: "Tormenta",
     shortDescription: "Controla el clima y es una de las líderes de los X-Men.",
-    description: "Tormenta tiene la capacidad de controlar y manipular el clima, y ha sido una líder destacada en los X-Men. Su presencia es fundamental en el equipo.",
+    description: "Tormenta, también conocida como Ororo Munroe, es una líder destacada en los X-Men con la extraordinaria capacidad de controlar y manipular el clima. Su papel es fundamental en el equipo, utilizando sus poderes para proteger a los mutantes y a la humanidad. Tormenta encarna la armonía entre humanos y mutantes, liderando con sabiduría y compasión en la lucha por la coexistencia pacífica.",
     imageUrl: "./imagesdata/tormenta2.jpg",
     facts: {
       alignment: "Héroe (X-Men)",
@@ -208,7 +172,7 @@ export default [
     id: "warpath",
     name: "Warpath",
     shortDescription: "Mutante con habilidades físicas mejoradas y habilidad de vuelo.",
-    description: "Warpath posee habilidades físicas mejoradas y la capacidad de volar, convirtiéndose en un formidable miembro de los X-Men.",
+    description: "Warpath, también conocido como James Proudstar, es un formidable mutante con habilidades físicas mejoradas y la capacidad de volar. Su presencia en los X-Men aporta una combinación única de fuerza y movilidad. Warpath desempeña un papel crucial en las misiones del equipo, enfrentándose a amenazas con valentía y demostrando que la diversidad de habilidades entre los mutantes fortalece la causa común de los X-Men.",
     imageUrl: "./imagesdata/warpath2.jpg",
     facts: {
       alignment: "Héroe (X-Men)",
@@ -220,7 +184,7 @@ export default [
     id: "william-stryker",
     name: "William Stryker",
     shortDescription: "Humano, militar y líder anti-mutante.",
-    description: "William Stryker es un líder militar anti-mutante que ve a los mutantes como una amenaza. Su papel a menudo lo coloca en conflicto con los X-Men.",
+    description: "William Stryker es un líder militar anti-mutante que percibe a los mutantes como una amenaza para la humanidad. Su ideología anti-mutante lo coloca en conflicto con los X-Men, liderando esfuerzos para contener y eliminar lo que ve como una peligrosa evolución. La historia de Stryker destaca las tensiones entre humanos y mutantes, explorando los prejuicios y el miedo que a menudo desencadenan conflictos en el universo de los X-Men.",
     imageUrl: "./imagesdata/William Stryker.jpg",
     facts: {
       alignment: "Villano",
@@ -232,7 +196,7 @@ export default [
     id: "wolverine",
     name: "Wolverine",
     shortDescription: "Tiene garras retráctiles y sentidos agudos.",
-    description: "Wolverine, también conocido como Logan, es conocido por sus garras retráctiles, sentidos agudos y habilidades regenerativas. Es un miembro fundamental de los X-Men.",
+    description: "Wolverine, también conocido como Logan, es un miembro fundamental de los X-Men conocido por sus garras retráctiles, sentidos agudos y habilidades regenerativas. A pesar de su fachada ruda, Wolverine lucha incansablemente por la justicia y la protección de los mutantes. Su historia, marcada por la experimentación y la inmortalidad relativa, añade complejidad a la narrativa de los X-Men, explorando la conexión entre la humanidad y la bestia interior que todos enfrentan.",
     imageUrl: "./imagesdata/wolverine2.jpg",
     facts: {
       alignment: "Héroe (X-Men)",
@@ -244,7 +208,7 @@ export default [
     id: "emma-frost",
     name: "Emma Frost",
     shortDescription: "Telepata y antigua villana convertida en X-Men.",
-    description: "Emma Frost es una poderosa telepata que ha pasado de ser una villana a ser una valiosa miembro de los X-Men. Su habilidad telepática es inigualable.",
+    description: "Emma Frost, también conocida como la Reina Blanca, es una poderosa telepata que ha pasado de ser una villana a ser una valiosa miembro de los X-Men. Su habilidad telepática es inigualable, permitiéndole comunicarse mentalmente y manipular pensamientos. Aunque su pasado está marcado por la ambigüedad moral, Emma Frost ahora abraza la causa mutante, contribuyendo con sus habilidades para proteger a aquellos que enfrentan persecución y discriminación.",
     imageUrl: "./imagesdata/emmafrost2.jpg",
     facts: {
       alignment: "Héroe (X-Men)",
@@ -256,7 +220,7 @@ export default [
     id: "rogue",
     name: "Rogue",
     shortDescription: "Tiene la habilidad de absorber los poderes, recuerdos y habilidades de cualquier persona que toque.",
-    description: "Rogue es un personaje de los X-Men con la capacidad de absorber los poderes, recuerdos y habilidades de cualquier persona que toque.",
+    description: "Rogue, también conocida como Anna Marie, es una poderosa mutante con la capacidad única de absorber los poderes, recuerdos y habilidades de cualquier persona que toque. Aunque esta habilidad la aísla de la intimidad física, Rogue utiliza sus dones para proteger a los demás. Su historia refleja la lucha por la aceptación y la conexión humana en un mundo donde el contacto puede ser tanto una bendición como una maldición.",
     imageUrl: "./imagesdata/rogue.jpg",
     facts: {
       alignment: "Héroe (X-Men)",
@@ -268,13 +232,12 @@ export default [
     id: "nightcrawler",
     name: "Nightcrawler",
     shortDescription: "Puede teletransportarse y tiene una apariencia azul y cola.",
-    description: "Nightcrawler tiene la habilidad de teletransportarse a cualquier lugar que pueda visualizar. Su apariencia azul y cola lo hacen inconfundible.",
+    description: "Nightcrawler, alias Kurt Wagner, es un héroe mutante con la increíble habilidad de teletransportarse a cualquier lugar visible. Su apariencia azul, cola prensil y carácter amable lo distinguen. Como miembro esencial de los X-Men, Nightcrawler equilibra sus dones sobrenaturales con un fuerte sentido ético, destacando en misiones de rescate y combate. Su historia refleja la lucha por la aceptación en una sociedad que a menudo teme lo diferente, aportando profundidad emocional a la narrativa de los X-Men.",
     imageUrl: "./imagesdata/Nightcrawler.jpg",
     facts: {
       alignment: "Héroe (X-Men)",
       gender: "Masculino",
       powers: "Teletransportación"
     }
-  },
+  }
 ];
-
